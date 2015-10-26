02666cf6aadecef39a0c214fb5e28b4b7989e391
Amy Bibbings <abibbings@cenmi.org>
2015-10-26 15:43:28 -0400

    added placeholder intro text and styling

--- wp-content/themes/_amuhlou2016/header.php
+++ wp-content/themes/_amuhlou2016/header.php
@@ -64,7 +64,18 @@
 		</div>
 	</div><!-- .container -->
 </nav><!-- .site-navigation -->
+<header id="masthead" class="site-header" role="banner">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	<div class="container">
+		<div class="row">
+			<div class="site-header-inner col-sm-12">
 
+				Hi. I'm Amy Bibbings. I make websites.
+
+			</div>
+		</div>
+	</div><!-- .container -->
+</header><!-- #masthead -->
 <div class="main-content">
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
 	<div class="container">
