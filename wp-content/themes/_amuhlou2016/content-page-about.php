b5b0ca533a89b147f4af418e9b928177acdc67ba
Amy Bibbings <abibbings@cenmi.org>
2015-11-24 12:29:55 -0500

    no message

---
+++ wp-content/themes/_amuhlou2016/content-page-about.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package _amuhlou2016
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+    <div class="entry-content">
+        <div class="entry-content-thumbnail">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <?php the_content(); ?>
+        <?php
+            wp_link_pages( array(
+                'before' => '<div class="page-links">' . __( 'Pages:', '_amuhlou2016' ),
+                'after'  => '</div>',
+            ) );
+        ?>
+    </div><!-- .entry-content -->
+    <?php edit_post_link( __( 'Edit', '_amuhlou2016' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+</article><!-- #post-## -->
--- wp-content/themes/_amuhlou2016/page-about.php
+++ wp-content/themes/_amuhlou2016/page-about.php
@@ -8,10 +8,13 @@ Template Name: About Page
 */
 
 get_header(); ?>
+<header>
+        <h1 class="page-title"><?php the_title(); ?></h1>
+    </header><!-- .entry-header -->
 <div class="col-md-7 col-md-offset-1 col-sm-12">
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php get_template_part( 'content', 'page' ); ?>
+		<?php get_template_part( 'content', 'page-about' ); ?>
 
 
 
